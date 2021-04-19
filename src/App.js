19a99715be80ef9ff0648926f00fb86d12c02adc
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-04-19 23:40:58 +0600

    added navlink and Style

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 
+import { NavLink } from 'react-router-dom';
 import './App.css';
-
+import fon from "./component/Imgs/logo.svg"
 function App() {
+  const activeLInk = {
+    fontWeight: "bold",
+  }
   return (
     <div className="App">
-      my first commit
+          <ul>
+        <NavLink to="/"><img src={fon} alt={"Estel"} width="100px" height="100px" /></NavLink>
+        <li><NavLink activeStyle={activeLInk} exact to="/home">Home</NavLink></li>
+        <li><NavLink activeStyle={activeLInk} to="/about">About</NavLink></li>
+        <li><NavLink activeStyle={activeLInk} to="/contact">Contackts</NavLink></li>
+        <li><NavLink activeStyle={activeLInk} to="catologs">Catologs</NavLink></li>
+      </ul>
     </div>
   );
 }
