d21f348331d519b02bcac841915fa0d00e8f3cb2
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-04-20 11:46:12 +0600

    Style and  vidio

---
+++ src/component/Home/Home.js
@@ -0,0 +1,23 @@
+import classes from "./Home.module.css";
+import audio from "../Audio/ESTELL.mp4"
+import HomeFull from "./HomeFull/HomeFull";
+import { Route } from "react-router";
+import HomeList from "./HomeList/HomeList";
+import { Link } from "react-router-dom";
+
+const Home = ({ match }) => {
+    return (<div>
+        <video src={audio} autoPlay loop
+
+            className={classes.video} />
+        <div className={classes.full}>
+            {match.isExact
+                ? <HomeList />
+                : <Route path="/home/:id" component={HomeFull} />}
+        </div>
+
+        <button className={classes.btn}><Link to="/catolog">Catolog</Link></button>
+    </div>);
+}
+
+export default Home;
