72a048b01f0bbb92744913eaf731ad13b9727ccb
ermek-tashtanbekov <tashtanbekovermek45@gmail.com>
2021-04-20 12:42:05 +0600

    added  new  Router

--- src/App.js
+++ src/App.js
@@ -5,7 +5,13 @@ import About from './component/About/About';
 import Catolog from './component/Catolog/Catolog';
 import Contakts from './component/Contakts/Contakts';
 import Home from './component/Home/Home';
+import Blondin from './component/Home/HomeName/Blondin/Blondin';
+import Hair from './component/Home/HomeName/ForBlondi/Hair/Hair';
+import HairDye from './component/Home/HomeName/HairDye/HairDye';
+import Men from './component/Home/HomeName/Men/Men';
+import Politra from './component/Home/HomeName/Politra/Politra';
 import fon from "./component/Imgs/logo.svg"
+import Stock from './component/Stock/Stock';
 function App() {
   const activeLInk = {
     fontWeight: "bold",
@@ -24,6 +30,12 @@ function App() {
         <Route path="/contacts" component={Contakts} />
         <Route path="/catologs" component={Catolog} />
         <Route path="/about" component={About} />
+        <Route  path="/men" component={Men}/>
+        <Route path="/catolog"  component={HairDye}/>
+        <Route  path="/catologg" component={Hair}/>
+        <Route  path="/politra" component={Politra}/>
+        <Route  path="/blondin" component={Blondin}/>
+        <Route  path="/stock" component={Stock}/>
       </Switch>
     </div>
   );
